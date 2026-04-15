9156ccb053dd1e725edcb9443cbf96782e7bab85
刘东升 <2873340975@qq.com>
2026-04-15 15:52:10 +0800

    Claude修改前的提交

--- src/api/doctor/doctorAppointment.ts
+++ src/api/doctor/doctorAppointment.ts
@@ -11,8 +11,13 @@ export interface ReservedPatientInfo {
   age: number
   deptName: string
   doctorName: string
+  cardId: string
+  // 是否已接诊，0前端显示未接诊，1前端显示已接诊
+  isReceive: 0 | 1
 }
 // 导出查询预约某个医生的患者信息表函数
 export const getReservedPatientsService = (): Promise<ApiResponse<ReservedPatientInfo[]>> => {
   return request.get(`/doctor/patientReserve`)
 }
+
+// 医生填写病历本需要传入的数据接口类型
